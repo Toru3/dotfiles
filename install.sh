c8aefb76cf7f9be95e8ae4786b5828f513748906
Toru3 <ryuutet@gmail.com>
2017-07-29 00:24:05 +0900

    install.shの修正

--- install.sh
+++ install.sh
@@ -1,30 +1,34 @@
-#!/bin/sh -x
+#!/bin/sh
+set -eu
+umask 0022
+export LC_ALL='C' PATH="$(command -p getconf PATH):$PATH"
+
 cd `dirname "${0}"`
 dir=`pwd`
-for f in .??*
+for file in `find -type f -name '.*' | sed 's/^\.\///' | grep -v /`
 do
-    test "$f" = ".git" && continue
-    if [ -e "${HOME}/$f" -a ! -L "${HOME}/$f" ]
+    if [ -e "${HOME}/$file" -a ! -L "${HOME}/$file" ]
     then
-        mv "${HOME}/$f" "${HOME}/${f}.old"
+        mv "${HOME}/$file" "${HOME}/${file}.old"
     fi
-    ln -sf "${dir}/$f" "${HOME}/$f"
+    ln -sf "${dir}/$file" "${HOME}/$file"
 done
 
 #vim
-mkdir -p ${HOME}/.vim
+mkdir -p "${HOME}/.vim"
 for dir in colors snippets syntax backup swap undo
 do
-    mkdir -p ${HOME}/.vim/$dir
+    mkdir -p "${HOME}/.vim/$dir"
 done
 
 for file in `find .vim -type f`
 do
-    ln -sf "${dir}/$f" "${HOME}/$f"
+    ln -sf "${dir}/$file" "${HOME}/$file"
 done
 
 if [ ! -d "${HOME}/.vim/dein" ]
 then
     curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
     sh installer.sh "${HOME}/.vim/dein"
+    vim -c 'call dein#install()' -c 'q'
 fi
